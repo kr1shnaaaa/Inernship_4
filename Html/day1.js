a633ae4d5514a4f763cec87278775cffcdb7be19
kr1shnaaaa <krishnadawalkar442@gmail.com>
2026-01-13 14:34:42 +0530

    fundamenals of javascript

---
+++ Html/day1.js
@@ -0,0 +1,57 @@
+console.log("Hello world");
+
+age = 25;
+
+name = "Krishna";
+
+
+
+
+console.log(age);
+console.log(name);
+
+Day=2;
+switch(Day)
+{    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    default:
+        console.log("Invalid input");
+}
+
+
+
+function hello()
+{
+    console.log("Hello World");
+}
+
+
+
+hello()
+
+ let arr = [1,2,3,4,5];
+    console.log(arr);
+
+
+    arr.push(6);
+
+    console.log(arr);
+
+    arr.pop();
+
+    console.log(arr);
+
+    
+
+
+    let Students = 
+    {name: "Krishna",
+     age: 25,
+     city: "Nashik"};
+
+    console.log(Students.name);
+    console.log(Students.age);
